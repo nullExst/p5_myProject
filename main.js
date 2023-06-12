b4fd3e433af3f22b78c7c7d2bc87a6f24215fa9c
Ykz57 <fave7241@gmail.com>
2023-06-12 21:22:46 +0900

    create main.js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 function setup(){
 	createCanvas(windowWidth, windowHeight);
-	frameRate(8);
+	frameRate(60);
 }
 
-function draw(){
-	background(33);
-	fill(255);
-	circle(width/2, height/2, 100);
+function draw() {
+  background(33);
+  fill(255);
+  circle(200,100,50);
+  fill(60);
+  square(200,200,100);
+  fill(120)
+  triangle(300, 200, 250, 300, 350, 300);
+  fill(255)
+  line(100,100,400,300);
+  
+  arc(300,150,100,100,0,100);
 }
